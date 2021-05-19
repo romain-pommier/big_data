21a1de221054b5dd4b0426bc1752721fc5806f23
romain <romain-p31@hotmail.fr>
2021-05-19 16:55:13 +0200

    tentative de fix connection avec sqlalchemy

--- app/app.py
+++ app/app.py
@@ -2,27 +2,41 @@
 from flask import Flask
 # import flask_sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import text
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from flask import jsonify
 
 config = {
-    'host' : 'localhost',
-    'port' : '3320',
-    'user' : 'newuser',
-    'password' : 'example',
-    'database' : 'classicmodels',
+    'host': 'db',
+    'port': 3306,
+    'user': 'root',
+    'password': 'example',
+    'database': 'classicmodels',
 }
 
 db_user = config.get('user')
 db_pwd = config.get('password')
-db_host = config.get('user')
-db_port = config.get('host')
+db_host = config.get('host')
+db_port = config.get('port')
 db_name = config.get('database')
 
 app = Flask(__name__)
 
-def create_app():
-    connection_str = 'mysql+mysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
-    engine = create_engine(connection_str)
-    connection = engine.connect()
+connection_str = f'mysql+pymysql://{db_user}:{db_pwd}@{db_host}:{db_port}/{db_name}'
+engine = create_engine(connection_str)
+connection = engine.connect()
+Session = sessionmaker(bind=engine)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Customer = Base.classes.customers
+Employee = Base.classes.employees
+Offices = Base.classes.offices
+OrderDetail = Base.classes.orderdetails
+Order = Base.classes.orders
+Payment = Base.classes.payments
+ProductLine = Base.classes.productlines
+Product = Base.classes.products
 
 @app.route('/')
 def index():
